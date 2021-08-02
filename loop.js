748b6aaf38395d70350ce3f7b6ade92e841b3d2a
BiplobHossainSobuj <bhs.cse@outlook.com>
2021-08-03 01:49:45 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,36 @@
+//while loop
+/*
+var javaScript = 0;
+while (javaScript<10) {
+    console.log(javaScript,"i will continue learning js");
+    javaScript++;
+}
+
+// problem -1
+var even = 0;
+while (even <=20) {
+    console.log(even);
+    even = even + 2;
+    
+}
+var odd = 1;
+while (odd <=20) {
+    console.log(odd);
+    odd = odd + 2;
+    
+}
+*/
+// for loop
+for(var number = 0; number<10; number++){
+    console.log(number);
+}
+// for(i=0;i<10;i++)
+for(i=1;i<10;i+=2){
+    console.log(i,"odd");
+}
+//
+var array = [23,35,6262,66,];
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
